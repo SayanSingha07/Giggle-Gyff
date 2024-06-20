4682311e57b686f4debfc0c79a0eb1babca1cce2
SayanSingha07 <sayansinghbabu143@gmail.com>
2024-06-20 19:06:36 +0530

    changes

--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 function AppLayout() {
   return(
-           <div> 
-        <h1>
-        AppLayout
-        </h1>
-           </div>
+           <>
+           <main>
+            <Outlet></Outlet>
+           </main>
+
+
+
+
+           </>
   )
   
 };
